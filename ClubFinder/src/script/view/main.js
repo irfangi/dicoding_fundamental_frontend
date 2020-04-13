8f424e4cb0650950c807c43fa34e0d1be716d732
Irfangi <irfangi564@gmail.com>
2020-04-13 14:19:22 +0700

    update module promise

--- ClubFinder/src/script/view/main.js
+++ ClubFinder/src/script/view/main.js
@@ -1,35 +1,42 @@
-var main = function () {
-    var searchElement = document.querySelector("#searchElement");
-    var buttonSearchElement = document.querySelector("#searchButtonElement");
-    var clubListElement = document.querySelector("#clubList");
+const main = () =>{
+    const searchElement = document.querySelector("#searchElement");
+    const buttonSearchElement = document.querySelector("#searchButtonElement");
+    const clubListElement = document.querySelector("#clubList");
 
-    var onButtonSearchClicked = function () {
-        var dataSource = new DataSource(renderResult, fallbackResult);
+    const onButtonSearchClicked = () => {
+        const dataSource = new DataSource(renderResult, fallbackResult);
         dataSource.searchClub(searchElement.value);
     };
 
-    var renderResult = function (results) {
+    const renderResult = (results) => {
         clubListElement.innerHTML = "";
-        results.forEach(function (club) {
-            var name = club.name;
-            var fanArt = club.fanArt;
-            var description = club.description;
+        results.forEach((club) => {
+            const {name, fanArt, description} = club;
+            // const name = club.name;
+            // const fanArt = club.fanArt;
+            // const description = club.description;
 
-            var clubElement = document.createElement("div");
+            const clubElement = document.createElement("div");
             clubElement.setAttribute("class", "club");
 
-            clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
-                '<div class="club-info">\n' +
-                '<h2>' + name + '</h2>\n' +
-                '<p>' + description + '</p>' +
-                '</div>';
+            // basic
+            // clubElement.innerHTML = '<img class="fan-art-club" src="' + fanArt + '" alt="Fan Art">\n' +
+            //     '<div class="club-info">\n' +
+            //     '<h2>' + name + '</h2>\n' +
+            //     '<p>' + description + '</p>' +
+            //     '</div>';
+
+            // es6
+            clubElement.innerHTML = `<img class="fan-art-club" src="${fanArt}"" alt="Fan Art">\n <div class="club-info">\n <h2>${name}</h2>\n <p>${description}</p> </div>`
+            
             clubListElement.appendChild(clubElement);
         })
     };
 
-    var fallbackResult = function (message) {
+    const fallbackResult = (message) => {
         clubListElement.innerHTML = "";
-        clubListElement.innerHTML += '<h2 class="placeholder">' + message + '</h2>'
+        
+        clubListElement.innerHTML += `<h2 class="placeholder"> ${message}</h2>`
     };
 
     buttonSearchElement.addEventListener("click", onButtonSearchClicked);
